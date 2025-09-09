40f9355e62193041b099ebb02ae2f59f7de223e2
Narong <admin@admins-MacBook-Air.local>
2025-09-09 23:45:06 +0200

    Create Employee class, getters, raiseSalary method
    In Emplyee class, we have two fields, name and salary(it has getters), and there is a method raiseSalary for raising the employee salary by input the percent(flaot) as an argument

---
+++ JavaRecap/Company.java
@@ -0,0 +1,12 @@
+package JavaRecap;
+
+
+public class Company {
+    public static void main(String[] args){
+        Employee e1 = new Employee("A", 1000.00);
+
+        e1.raiseSalary(0.1);
+
+        System.out.print(e1.getSalary());
+    }
+}
